cc09de7c935dc5d065a357dc52ccf90482461087
Naohide Sano <namachapanda2005@gmail.com>
2022-09-30 14:42:52 +0900

    [servlet] follow library update

--- src/main/java/vavi/net/ods/Image.java
+++ src/main/java/vavi/net/ods/Image.java
@@ -14,10 +14,10 @@ import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 public class Image extends HttpServlet {
